504ca7e3ad245aa478867901dc31d2ad73e5ae94
Benjamin Day <benday@benday.com>
2024-03-03 12:17:42 -0500

    migrated old demo code

---
+++ Benday.Presidents/src/Benday.Presidents.WebUi/Startup.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Benday.Presidents.WebUi.Data;
+using Benday.Presidents.WebUi.Models;
+using Benday.Presidents.WebUi.Services;
+using Microsoft.AspNetCore.Http;
+using Benday.Presidents.Api.Interfaces;
+using Benday.Presidents.Api.Services;
+using Benday.Presidents.Api.Features;
+using Benday.Presidents.Api.DataAccess;
+using Benday.Presidents.Api.DataAccess.SqlServer;
+using Benday.Presidents.Common;
+
+namespace Benday.Presidents.WebUi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("default")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(
+                options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequiredLength = 4;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            
+            RegisterTypes(services);
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=President}/{action=Index}/{id?}");
+            });
+        }
+
+        void RegisterTypes(IServiceCollection services)
+        {
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+            // services.AddTransient<IEmailSender, AuthMessageSender>();
+            // services.AddTransient<ISmsSender, AuthMessageSender>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddTransient<IUsernameProvider, HttpContextUsernameProvider>();
+
+            services.AddTransient<IFeatureManager, FeatureManager>();
+
+            services.AddTransient<Api.Services.ILogger, Logger>();
+
+            services.AddDbContext<PresidentsDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("default")));
+
+            services.AddTransient<IPresidentsDbContext, PresidentsDbContext>();
+
+            services.AddTransient<IRepository<Person>, SqlEntityFrameworkPersonRepository>();
+            services.AddTransient<IPresidentValidatorStrategy, PresidentValidatorStrategy>();
+
+            services.AddTransient<IFeatureRepository, SqlEntityFrameworkFeatureRepository>();
+
+            var tempServiceProvider = services.BuildServiceProvider();
+
+            var features = tempServiceProvider.GetService<IFeatureManager>();
+
+            if (features.FeatureUsageLogging == false)
+            {
+                services.AddTransient<IPresidentService, PresidentService>();
+            }
+            else
+            {
+                services.AddTransient<IPresidentService, PresidentServiceWithLogging>();
+            }
+        }
+    }
+}
---
+++ Benday.Presidents/src/Benday.Presidents.WebUi/gulpfile.js
@@ -0,0 +1,123 @@
+﻿/// <binding BeforeBuild='clean, default' Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    htmlmin = require("gulp-htmlmin"),
+    uglify = require("gulp-uglify"),
+    merge = require("merge-stream"),
+    del = require("del"),
+    bundleconfig = require("./bundleconfig.json");
+
+var regex = {
+    css: /\.css$/,
+    html: /\.(html|htm)$/,
+    js: /\.js$/
+};
+
+gulp.task("min", ["min:js", "min:css", "min:html"]);
+
+gulp.task("min:js", function () {
+    var tasks = getBundles(regex.js).map(function (bundle) {
+        return gulp.src(bundle.inputFiles, { base: "." })
+            .pipe(concat(bundle.outputFileName))
+            .pipe(uglify())
+            .pipe(gulp.dest("."));
+    });
+    return merge(tasks);
+});
+
+gulp.task("min:css", function () {
+    var tasks = getBundles(regex.css).map(function (bundle) {
+        return gulp.src(bundle.inputFiles, { base: "." })
+            .pipe(concat(bundle.outputFileName))
+            .pipe(cssmin())
+            .pipe(gulp.dest("."));
+    });
+    return merge(tasks);
+});
+
+gulp.task("min:html", function () {
+    var tasks = getBundles(regex.html).map(function (bundle) {
+        return gulp.src(bundle.inputFiles, { base: "." })
+            .pipe(concat(bundle.outputFileName))
+            .pipe(htmlmin({ collapseWhitespace: true, minifyCSS: true, minifyJS: true }))
+            .pipe(gulp.dest("."));
+    });
+    return merge(tasks);
+});
+
+gulp.task("clean", function () {
+    var files = bundleconfig.map(function (bundle) {
+        return bundle.outputFileName;
+    });
+
+    return del(files);
+});
+
+gulp.task("watch", function () {
+    getBundles(regex.js).forEach(function (bundle) {
+        gulp.watch(bundle.inputFiles, ["min:js"]);
+    });
+
+    getBundles(regex.css).forEach(function (bundle) {
+        gulp.watch(bundle.inputFiles, ["min:css"]);
+    });
+
+    getBundles(regex.html).forEach(function (bundle) {
+        gulp.watch(bundle.inputFiles, ["min:html"]);
+    });
+});
+
+function getBundles(regexPattern) {
+    return bundleconfig.filter(function (bundle) {
+        return regexPattern.test(bundle.outputFileName);
+    });
+}
+
+
+
+
+// borrowed from Shawn Wildermuth
+// https://wildermuth.com/2017/11/19/ASP-NET-Core-2-0-and-the-End-of-Bower
+
+// Dependency Dirs
+var deps = {
+    "jquery": {
+        "dist/*": "dist"
+    },
+    "jquery-validation": {
+        "dist/*": "dist"
+    },
+    "jquery-validation-unobtrusive": {
+        "*": ""
+    },
+    "bootstrap": {
+        "dist/**/*": "dist"
+    },
+    "font-awesome": {
+        "css/*": "css",
+        "fonts/*": "fonts"
+    },
+    "popper.js": {
+        "dist/*": "dist"
+    },
+};
+
+gulp.task("scripts", function () {
+
+    var streams = [];
+    for (var prop in deps) {
+        console.log("Prepping Scripts for: " + prop);
+        for (var itemProp in deps[prop]) {
+            streams.push(gulp.src("node_modules/" + prop + "/" + itemProp)
+                .pipe(gulp.dest("wwwroot/lib/" + prop + "/" + deps[prop][itemProp])));
+        }
+    }
+
+    return merge(streams);
+
+});
+
+gulp.task("default", ['clean', 'min', 'scripts']);
